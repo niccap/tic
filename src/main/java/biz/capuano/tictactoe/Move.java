a23127fa06fe7e8a7b3c930ed475a8d667b9f694
niccap <nicola@capuano.biz>
2023-11-15 17:02:12 +0100

    Revert "Modified move.java, added variable x"
    This reverts commit 06d6d6d4da09121394aa64ba7f09c98bd6dae65a.

--- src/main/java/biz/capuano/tictactoe/Move.java
+++ src/main/java/biz/capuano/tictactoe/Move.java
@@ -3,12 +3,10 @@ package biz.capuano.tictactoe;
 class Move {
     private final int row;
     private final int col;
-    private int x;
 
     public Move(int row, int col) {
         this.row = row;
         this.col = col;
-        this.x = 0;
     }
 
     public int getRow() {
