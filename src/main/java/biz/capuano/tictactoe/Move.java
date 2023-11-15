06d6d6d4da09121394aa64ba7f09c98bd6dae65a
niccap <nicola@capuano.biz>
2023-11-15 16:57:02 +0100

    Modified move.java, added variable x

--- src/main/java/biz/capuano/tictactoe/Move.java
+++ src/main/java/biz/capuano/tictactoe/Move.java
@@ -3,10 +3,12 @@ package biz.capuano.tictactoe;
 class Move {
     private final int row;
     private final int col;
+    private int x;
 
     public Move(int row, int col) {
         this.row = row;
         this.col = col;
+        this.x = 0;
     }
 
     public int getRow() {
